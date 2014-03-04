133e741304f971bdfcfba831fc95b9d6caaca418
Neha Kumari <bodhisattva@Prashanths-iMac-2.local>
2014-03-03 19:43:25 -0800

    Add seed data for customers and charges

--- source/culini/db/seeds.rb
+++ source/culini/db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#seeding customers
+customer_1 = {:first_name => "Dean", :last_name => "Martin", :email => "dean.martin@culini.com"}
+customer_2 = {:first_name => "Buddy", :last_name => "Rich", :email => "buddy.rich@culini.com"}
+customer_3 = {:first_name => "Miles", :last_name => "Davis", :email => "miles.davis@culini.com"}
+customer_4 = {:first_name => "Billie", :last_name => "Holiday", :email => "billie.holiday@culini.com"}
+
+customers = []
+
+[customer_1, customer_2, customer_3, customer_4].each do |customer|
+	customers << Customer.create(customer)
+end
+
+#linking charges
+
+#successfull charges
+successful_charge_params = {paid: true, refunded: false, currency: "usd"}
+5.times { Charge.create(successful_charge_params.merge({customer_id: customers[0], amount: rand(1000)}))}
+3.times { Charge.create(successful_charge_params.merge({customer_id: customers[1], amount: rand(1000)}))}
+1.times { Charge.create(successful_charge_params.merge({customer_id: customers[2], amount: rand(1000)}))}
+1.times { Charge.create(successful_charge_params.merge({customer_id: customers[3], amount: rand(1000)}))}
+
+#failed charges
+failed_charge_params = {paid: false, refunded: false, currency: "usd"}
+3.times { Charge.create(failed_charge_params.merge({customer_id: customers[2], amount: rand(1000)}))}
+2.times { Charge.create(failed_charge_params.merge({customer_id: customers[3], amount: rand(1000)}))}
+
+#disputed charges
+disputed_charge_params = {paid: true, refunded: true, currency: "usd"}
+3.times { Charge.create(disputed_charge_params.merge({customer_id: customers[0], amount: rand(1000)}))}
+2.times { Charge.create(disputed_charge_params.merge({customer_id: customers[1], amount: rand(1000)}))}
