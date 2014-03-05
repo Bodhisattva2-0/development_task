e8f1401d336f8a296a2ed3dfc333fe63d98c17b2
Neha Kumari <bodhisattva@Prashanths-iMac-2.local>
2014-03-04 18:04:32 -0800

    Add a validation for currency being in usd, inr etc

--- source/culini/app/models/charge.rb
+++ source/culini/app/models/charge.rb
@@ -2,4 +2,5 @@ class Charge < ActiveRecord::Base
   CURRENCIES = ["usd", "gbp", "eur", "inr"]
   belongs_to :customer
   validates_presence_of :amount,  :currency, :customer_id
+  validates :currency, :inclusion => CURRENCIES
 end
--- source/culini/spec/models/charge_spec.rb
+++ source/culini/spec/models/charge_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe Charge do
+  describe  "validations" do
+    it "should make sure currency value is valid" do
+      charge = FactoryGirl.build(:charge, :currency => "foo")
+      expect(charge).to be_invalid
+    end
+  end
 end
